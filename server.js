a532759b04d8dca1c1cbd7fdb0ec3c37db70c446
Alyson May <amay1421@yahoo.com>
2021-02-02 17:17:54 -0600

    added listener

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
+//Routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+
+//Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
