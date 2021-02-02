70761a65000eab2e466a636c88d344baf8e59e48
Alyson May <amay1421@yahoo.com>
2021-02-02 17:59:53 -0600

    added the post

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var app = express();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 //Routes
@@ -16,9 +16,21 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
+app.post("/api/notes", function(req, res) {
+    var postNote = req.body;
+    var dbData = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json")));
+    dbData.push(postNote)
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(dbData));
+
+    res.json(dbData)
+});
+
+
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
   });
+
+
 
 //Listener
 app.listen(PORT, function() {
